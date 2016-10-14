20f4cec6b91a7e47aa6489ba5fec264c4201cb8f
robati <robati.m7@gmail.com>
2016-10-14 19:30:19 +0330

    Create constants.cpp

---
+++ Bownling/constants.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+enum constants{
+	totalrolls=2,totalframes=12,allpins=10,normalFrames=10,firstRoll=0,lastRoll=1
+};
+
+enum frametype{
+	strike,spare,openframe
+};
